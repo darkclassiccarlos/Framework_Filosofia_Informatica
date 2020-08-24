66157b8a97713c8edd90795f73adaa14eccb13f1
Carlos <carlosand_01@hotmail.com>
2020-08-23 20:43:28 -0500

    actualizar

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const http = require('http');
+const showdown = require('showdown');
 const path = require('path');
 const app = express();
 const server = http.Server(app);
 const io = require('socket.io')(server);
+const converter = new showdown.Converter();
 
 // ---------------------------------------------------------------------------
 // Configuration
@@ -24,8 +26,26 @@ app.get('/edit', (req, res) => {
 // Socket Events
 io.on('connection', (socket) => {
     console.log(`[ server.js ] ${socket.id} connected`);
-    socket.emit('update slide', `Hello ${socket.id}`);
     socket.on('disconnect', () => {
         console.log(`[ server.js ] ${socket.id} disconnected`);
     });
+});
+
+function updateSlide(markdown) {
+    io.emit('update slide', converter.makeHtml(markdown));
+}
+
+// API
+
+app.get('/api/updateSlide', (req, res) => {
+    console.log(`[ server.js ] GET request to 'api/updateSlide' => ${JSON.stringify(req.query)}`);
+
+    const { markdown } = req.query;
+
+    if (markdown) {
+        updateSlide(markdown);
+        res.status(200).send(`Received 'updateSlide' request with: ${markdown}\n`);
+    } else {
+        res.status(400).send('Invalid parameters.\n');
+    }
 });
